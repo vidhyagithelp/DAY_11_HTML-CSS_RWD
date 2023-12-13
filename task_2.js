565c17a3e5a8f5a7d35b500bd63a9d5b0486acdc
vidhyagithelp <vidhya26.d@gmail.com>
2023-12-13 22:23:29 +0530

    first commit

---
+++ task_2.js
@@ -0,0 +1,61 @@
+// // document.addEventListener("DOMContentLoaded" ,()=>{
+// //     // let leftSection=document.getElementsByClassName("left");
+
+// //     // console.log(leftSection);
+// //     // let divElement=document.createElement("div");
+// //     // divElement.style.backgroundColor="red";
+// //     // divElement.style.height= "100px";
+// //     // document.body.append(divElement);
+// //     // console.log(divElement);
+
+// //     let number=10;
+// // for(let i=1;i<=number;i++){
+// //     let divElement=document.createElement("div");
+// //     divElement.style.backgroundColor="red";
+// //     divElement.style.height= "100px";
+// //     divElement.style.marginBottom= "10px";
+// //     divElement.innerHTML= i;
+// //      let leftSection=document.getElementById("right");
+// //      console.log(leftSection);
+// //      leftSection.append(divElement);
+// //      document.body.append(leftSection);
+// // }
+
+// // })
+
+// let images = [
+//   // "https://www.pexels.com/photo/blue-green-and-brown-peacock-65894/",
+//   "https://images.pexels.com/photos/1212693/pexels-photo-1212693.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   "https://images.pexels.com/photos/1906658/pexels-photo-1906658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   "https://images.pexels.com/photos/2260800/pexels-photo-2260800.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   "https://images.pexels.com/photos/1252811/pexels-photo-1252811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   "https://images.pexels.com/photos/1999579/pexels-photo-1999579.jpeg?auto=compress&cs=tinysrgb&w=600",
+//   "https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   // "https://www.pexels.com/photo/sunflower-selective-focus-photography-1366630/",
+//   // "https://www.pexels.com/photo/buildings-near-body-of-water-at-night-1519088/",
+//   // "https://www.pexels.com/photo/top-view-photo-of-strawberries-1266741/",
+// ];
+
+// let divCollection=["div","div","div","div"];
+// // let maindivElement=document.createElement("div");
+// //rightDiv
+// let containerDiv = document.getElementById("container");
+
+
+// let rightSection = document.getElementById("right"); //right div
+// console.log(rightSection);
+
+// for(let j=0;j<divCollection.length;j++){
+//   let divElement = document.createElement("div"); //div1
+//   divElement.setAttribute("id", "flexDiv"); //div
+
+// for (let i = 0; i < images.length; i++) {
+//   let imgElement = document.createElement("img"); //img
+//   imgElement.src = images[i];
+//   imgElement.style.width = "100%";
+//   divElement.append(imgElement);
+// }
+// rightSection.append(divElement);
+// containerDiv.append(rightSection);
+
+// }
